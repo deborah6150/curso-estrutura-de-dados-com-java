11b82653afd7a2d1dcdf07dccd3f64acdf4c6cac
Deborah Caroline <deborahcaroline615@gmail.com>
2022-01-30 12:53:43 -0300

    commit referente as aulas 1 ao 3 do curso

---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,17 @@
+package com.deborah.estruturadados.vetor.teste;
+
+import com.deborah.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+		
+	}
+
+}
---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,23 @@
+package com.deborah.estruturadados.vetor.teste;
+
+import com.deborah.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(2);
+		
+		try {
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 3");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+}
