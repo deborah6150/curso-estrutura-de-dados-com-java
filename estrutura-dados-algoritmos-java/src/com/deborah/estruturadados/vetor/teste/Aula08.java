6cb3874f89e809e6af41f5aebf9ba11683e686e0
Deborah Caroline <deborahcaroline615@gmail.com>
2022-01-31 12:03:20 -0300

    commit referente a aula 08 metodo para aumentar a capacidade do vetor

---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/vetor/teste/Aula08.java
@@ -0,0 +1,21 @@
+package com.deborah.estruturadados.vetor.teste;
+
+import com.deborah.estruturadados.vetor.Vetor;
+
+public class Aula08 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(3);
+		
+		vetor.adiciona("B");
+		vetor.adiciona("C");
+		vetor.adiciona("E");
+		vetor.adiciona("F");
+		vetor.adiciona("G");
+		
+		System.out.println(vetor);
+
+	}
+
+}
