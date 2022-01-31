3d2d5754c9294c9fb260772e09936e1f81c81fd5
Deborah Caroline <deborahcaroline615@gmail.com>
2022-01-31 12:21:14 -0300

    commit referente a aula 09 metodo para remover elementos do vetor

---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/vetor/teste/Aula09.java
@@ -0,0 +1,37 @@
+package com.deborah.estruturadados.vetor.teste;
+
+import com.deborah.estruturadados.vetor.Vetor;
+
+public class Aula09 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(3);
+		
+		vetor.adiciona("B");
+		vetor.adiciona("C");
+		vetor.adiciona("E");
+		vetor.adiciona("F");
+		vetor.adiciona("G");
+		
+		System.out.println(vetor);
+		
+		vetor.remove(1);
+		
+		System.out.println(vetor);
+		
+		System.out.println("Remover o elemento E");
+		
+		int pos = vetor.busca("E");
+		if(pos > -1) {
+			vetor.remove(pos);
+		}
+		else {
+			System.out.println("Elemento nao existe no vetor");
+		}
+		
+		System.out.println(vetor);
+
+	}
+
+}
