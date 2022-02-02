c277c68febefcde28cc070f47a3fe3eb7d83dbcb
Deborah Caroline <deborahcaroline615@gmail.com>
2022-02-02 20:08:44 -0300

    commit referente a aula 13 pilha

---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/base/EstruturaEstatica.java
@@ -0,0 +1,99 @@
+package com.deborah.estruturadados.base;
+
+import java.lang.reflect.Array;
+
+public class EstruturaEstatica<T> {
+
+	private T[] elementos;
+	private int tamanho;
+	
+	public EstruturaEstatica(){
+		this(10);
+	}
+
+	public EstruturaEstatica (int capacidade) {
+		this.elementos = (T[]) new Object[capacidade];
+		this.tamanho = 0;
+	}
+
+	public EstruturaEstatica (int capacidade, Class<T> tipoClasse) {
+		this.elementos = (T[]) Array.newInstance(tipoClasse, capacidade);
+		this.tamanho = 0;
+	}
+
+	protected boolean adiciona(T elemento) {
+
+		this.aumentaCapacidade();
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+
+	protected boolean adiciona(int posicao ,T elemento) {
+
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posicao invalida");
+		}
+
+		this.aumentaCapacidade();
+
+		//mover todos os elementos
+		for(int i=this.tamanho-1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+
+		return true;
+	}
+
+	private void aumentaCapacidade() {
+		if(this.tamanho == this.elementos.length) {
+			T[] elementosNovos = (T[]) new Object[this.elementos.length * 2];
+			for(int i=0; i<this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	
+	protected void remove(int posicao){
+		if (!(posicao >= 0 && posicao < tamanho)){
+			throw new IllegalArgumentException("Posicao inválida");
+		}
+		for (int i=posicao; i<tamanho-1; i++){
+			elementos[i] = elementos[i+1];
+		}
+		tamanho--;
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		for(int i=0; i<this.tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(" , ");
+		}
+
+		if(this.tamanho>0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+
+		s.append("]");
+
+		return s.toString();
+	}
+
+}
---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/pilha/Pilha.java
@@ -0,0 +1,15 @@
+package com.deborah.estruturadados.pilha;
+
+import com.deborah.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+	
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+
+}
