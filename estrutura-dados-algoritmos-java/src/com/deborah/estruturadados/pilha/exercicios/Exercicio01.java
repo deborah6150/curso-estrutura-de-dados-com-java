758142ab897ce3cf89ac45fb28e3b30aafbf8e20
Deborah Caroline <deborahcaroline615@gmail.com>
2022-02-02 21:43:31 -0300

    commit referente ao exercicio 1 pilha - empilha e desempilha

---
+++ estrutura-dados-algoritmos-java/src/com/deborah/estruturadados/pilha/exercicios/Exercicio01.java
@@ -0,0 +1,48 @@
+package com.deborah.estruturadados.pilha.exercicios;
+
+import java.util.Scanner;
+
+import com.deborah.estruturadados.pilha.Pilha;
+
+
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Pilha<Integer> pilha = new Pilha<>();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i=1; i<=10; i++) {
+			
+			System.out.println("Entre com um numero: \n\n");
+			int num = scan.nextInt();
+			
+			if (num % 2 == 0) {
+				System.out.println("Empilhando o numero " + num);
+				pilha.empilha(num);
+			}
+			else {
+				
+				Integer desempilhado = pilha.desempilha();
+				
+				if (desempilhado == null) {
+					System.out.println("Pilha esta vazia :(");
+				}else {
+					System.out.println("Numero impar, desempilhando um elemento da pilha" + desempilhado);
+				}
+					
+			}
+		}
+		System.out.println("Todos os numeros foram lidos, desempilhando numeros da pilha");
+		
+		while(!pilha.estaVazia()) {
+			
+				System.out.println("Desempilhando um elemento da pilha" + pilha.desempilha());
+			
+		}
+		System.out.println("Todos os elementos foram desempilhados");
+	}
+
+}
